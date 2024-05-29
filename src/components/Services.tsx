59c10cbe44e11050816bb97e94a32005c43884ff
LIGAMD\rcarmona <randolph.carmona@lmd.gob.do>
2024-05-29 13:08:03 -0400

    feat: Add Divider component and update LandingPage with new design

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,7 +2,71 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className="h-screen py-5 bg-white dark:bg-gray-900 mt-10">
+       <div className="container flex flex-col items-center justify-center p-6 mx-auto mt-4 text-center xl:px-0">
+        <div className="text-sm font-bold tracking-wider text-indigo-600 uppercase">Testimonials</div>
+        <h2 className="max-w-2xl mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl dark:text-white">Here&#x27;s what our customers said</h2>
+        <p className="max-w-2xl py-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300">Testimonails is a great way to increase the brand trust and awareness. Use this section to highlight your popular customers.</p>
+    </div>
+    <div className="container max-w-screen-xl px-4 mx-auto my-10" id="features">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+            <div className="my-5 grid__item lg:m-5">
+                <div className="flex items-center justify-center text-indigo-600 bg-indigo-100 rounded-full w-14 h-14 dark:bg-indigo-900 dark:text-indigo-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" className="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"></path>
+                    </svg>
+                </div>
+                <h3 className="pt-2 my-4 text-xl font-medium text-black dark:text-white text-opacity-90">Simple Code base</h3>
+                <p className="text-lg text-gray-700 dark:text-gray-400 text-opacity-70">Alpha low hanging fruit focus burn rate niche market accelerator advisor agile development marketing.</p>
+            </div>
+            <div className="my-5 grid__item lg:m-5">
+                <div className="flex items-center justify-center text-indigo-600 bg-indigo-100 rounded-full w-14 h-14 dark:bg-indigo-900 dark:text-indigo-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" className="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"></path>
+                    </svg>
+                </div>
+                <h3 className="pt-2 my-4 text-xl font-medium text-black dark:text-white text-opacity-90">Powerful Databases</h3>
+                <p className="text-lg text-gray-700 dark:text-gray-400 text-opacity-70">Ramen equity strategy ownership. Angel investor holy grail growth hacking ownership traction rockstar startup.</p>
+            </div>
+            <div className="my-5 grid__item lg:m-5">
+                <div className="flex items-center justify-center text-indigo-600 bg-indigo-100 rounded-full w-14 h-14 dark:bg-indigo-900 dark:text-indigo-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" className="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
+                    </svg>
+                </div>
+                <h3 className="pt-2 my-4 text-xl font-medium text-black dark:text-white text-opacity-90">Easy Login &amp; Access</h3>
+                <p className="text-lg text-gray-700 dark:text-gray-400 text-opacity-70">Founders holy grail lean startup gamification hypotheses. Series financing disruptive branding.</p>
+            </div>
+            <div className="my-5 grid__item lg:m-5">
+                <div className="flex items-center justify-center text-indigo-600 bg-indigo-100 rounded-full w-14 h-14 dark:bg-indigo-900 dark:text-indigo-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" className="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"></path>
+                    </svg>
+                </div>
+                <h3 className="pt-2 my-4 text-xl font-medium text-black dark:text-white text-opacity-90">Design without Limits</h3>
+                <p className="text-lg text-gray-700 dark:text-gray-400 text-opacity-70">Early adopters funding innovator series A financing business plan pitch metrics venture branding and company.</p>
+            </div>
+            <div className="my-5 grid__item lg:m-5">
+                <div className="flex items-center justify-center text-indigo-600 bg-indigo-100 rounded-full w-14 h-14 dark:bg-indigo-900 dark:text-indigo-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" className="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"></path>
+                    </svg>
+                </div>
+                <h3 className="pt-2 my-4 text-xl font-medium text-black dark:text-white text-opacity-90">Cloud Powered</h3>
+                <p className="text-lg text-gray-700 dark:text-gray-400 text-opacity-70">Crowdsource ramen crowdfunding backing. Business model canvas responsive web design non-disclosure.</p>
+            </div>
+            <div className="my-5 grid__item lg:m-5">
+                <div className="flex items-center justify-center text-indigo-600 bg-indigo-100 rounded-full w-14 h-14 dark:bg-indigo-900 dark:text-indigo-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" className="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path>
+                    </svg>
+                </div>
+                <h3 className="pt-2 my-4 text-xl font-medium text-black dark:text-white text-opacity-90">GDPR Compliant</h3>
+                <p className="text-lg text-gray-700 dark:text-gray-400 text-opacity-70">Virality influencer assets lean startup interaction design beta alpha seed round launch party.</p>
+            </div>
+        </div>
+    </div>
+</div>
   )
 }
 
