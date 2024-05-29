ea9fbbc87bd20e03cbfc55f430c778fb063e4fcd
LIGAMD\rcarmona <randolph.carmona@lmd.gob.do>
2024-05-29 09:55:50 -0400

    chore: Add new components to Home page
    ```

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import Contact from "@/components/Contact";
+import LandingPage from "@/components/LandingPage";
+import NavBar from "@/components/NavBar";
+import Services from "@/components/Services";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
     <main>
-      <h1>Formated</h1>
+      <NavBar />
+      <LandingPage />
+      <Services />
+      <Testimonials/>
+      <Contact />
     </main>
   );
 }
